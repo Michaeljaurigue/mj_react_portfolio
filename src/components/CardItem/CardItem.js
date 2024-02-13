62b48223e666334c0d6c0b0eb41a56b05c7f89b7
Michael Jaurigue <112568096+Michaeljaurigue@users.noreply.github.com>
2024-02-13 09:00:26 -0600

    Update to CardItem

--- src/components/CardItem/CardItem.js
+++ src/components/CardItem/CardItem.js
@@ -10,7 +10,7 @@ function CardItem(props) {
             <img src={props.src} alt="Project" className="cards__item__img" />
           </figure>
           <div className="cards__item__info">
-            <h5 className="cards__item__text">{props.text}</h5>
+            <h5 className="cards__item__text">{props.text}</h5> // Project Name
           </div>
         </Link>
       </li>
