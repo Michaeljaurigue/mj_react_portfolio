eac7806117a005595d6412daf922b251962017d8
Michael Jaurigue <112568096+Michaeljaurigue@users.noreply.github.com>
2024-02-16 12:45:01 -0600

    Update: Project List

--- src/components/pages/Projects/Projects.js
+++ src/components/pages/Projects/Projects.js
@@ -13,13 +13,13 @@ function Cards() {
               src="images/se_project_aroundtheus.jpg"
               text="React | API | Node.js | Git :  Allows users to add and delete cards, customize profile picture, and change the name of the user. Click the image to see the live site!"
               label="Around the US"
-              path="https://github.com/Michaeljaurigue/se_project_aroundtheus"
+              path="https://aroundtheuswithmj.netlify.app/"
             />
             <CardItem
               src="images/se_project_wtwr_react.jpg"
               text="React | API | Express | MongoDB | Node.js | Git : This app uses API to recommend an outfit based on the weather. Click the image to see the live site!"
               label="WTWR Weather Outfit App"
-              path="https://michaeljaurigue.github.io/se_project_react/"
+              path="https://wtwr.devwonders.com/main"
             />
           </ul>
           <ul className="cards__items">
@@ -37,9 +37,21 @@ function Cards() {
             />
             <CardItem
               src="images/mj_nachicago.jpg"
-              text="React | API | Express | MongoDB | Node.js | Git : Currently redeveloping this website. Visit the website to see the current version!"
+              text="Utilize Wix to help with the UI/UX and SEO for a non-profit organization. Click the image to see the live site!"
               label="New Acropolis Chicago Website"
               path="https://www.nachicago.org/"
+            />
+            <CardItem
+              src="images\sparkysteps michael jaurigue website.PNG"
+              text="Utilize Shopify web builder to manage, design and maintain this website and business! Click the image to see the live site!"
+              label="Sparky Steps Website"
+              path="https://sparkysteps.com/"
+            />
+            <CardItem
+              src="images\cuethehumans.PNG"
+              text="Utilize Shopify web builder to manage, design and maintain this philosophical blog! Click the image to see the live site!"
+              label="Cue The Humans Blog"
+              path="https://cuethehumans.com/"
             />
             <CardItem
               src="images/mj_globaldotmedia.jpg"
