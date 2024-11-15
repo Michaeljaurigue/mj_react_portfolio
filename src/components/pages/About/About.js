95b2c80ad843efed10484f47c049682a539f4e0a
Michael Jaurigue <112568096+Michaeljaurigue@users.noreply.github.com>
2024-11-15 09:02:29 -0600

    Update: About.js

--- src/components/pages/About/About.js
+++ src/components/pages/About/About.js
@@ -12,22 +12,22 @@ export default function About() {
       </div>
       <div className="about__container-text">
         <p>
-          I’m a Pet Sitter, Artist, Brand Manager, Digital Marketer, Web Developer, Philosopher, and Graphic Designer dedicated to creating meaningful experiences. From pet care to building user-friendly websites, crafting impactful designs, and running effective marketing campaigns, I help individuals and businesses achieve their goals.
+          I’m Michael Jaurigue—a Pet Sitter, Artist, Brand Manager, Digital Marketer, Web Developer, Philosopher, and Graphic Designer with a passion for creating meaningful experiences. From caring for pets to building user-friendly websites, designing impactful visuals, and running successful marketing campaigns, I help individuals and businesses achieve their goals.
         </p>
       </div>
       <div className="about__container-text">
         <p>
-          As the founder of Sparky Steps, I’ve built a successful pet care brand while mastering the art of connecting with audiences through design and digital solutions. I’m passionate about using creativity and technology to deliver results.
+          As the founder of Sparky Steps, I’ve built a thriving pet care brand while mastering the art of audience connection through creative design and digital solutions. My work is driven by a deep love for art, innovation, and social entrepreneurship, combining creativity and technology to deliver results that inspire and engage.
         </p>
       </div>
       <div className="about__container-text">
         <p>
-          Driven by a love for art, digital innovation, and social entrepreneurship, I aim to create solutions that inspire, engage, and make a positive impact.
+          I believe in the power of collaboration and the positive impact of purposeful solutions.
         </p>
       </div>
       <div className="about__container-text">
         <p>
-          Let’s collaborate to bring your ideas to life and make a difference together. 🌎
+          Let’s bring your ideas to life and make a meaningful difference—together. 🌟🌎
         </p>
         <br />
         <br />
