b3959736ceaf6c35daf0e4a870e9512bf49856a8
Michael Jaurigue <112568096+Michaeljaurigue@users.noreply.github.com>
2024-11-15 08:57:48 -0600

    Update: Home.js

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -1,6 +1,5 @@
 import React from "react";
 import "../../App/App.css";
-import Cards from "../Projects/Projects";
 import HeroSection from "../../HeroSection/HeroSection";
 import Footer from "../../Footer/Footer";
 import About from "../About/About";
